71d7356a2b691b2b710d0c37f7a41bd93f3bfef2
h669800 <111427835+h669800@users.noreply.github.com>
2022-09-03 12:30:14 +0200

    Update J2minTal.java

--- Uke-34/src/oppgåver/J2minTal.java
+++ Uke-34/src/oppgåver/J2minTal.java
@@ -5,23 +5,20 @@ import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
 
 public class J2minTal {
-
+	
 	public static void main(String[] args) {
 		String input = showInputDialog(
 				"Dette programmet finner det minste tallet \n "
 				+ "Eksempel: 1,4,3,8 = 1, eller 2,7 = 2 \n"
 				+ " Skriv inn talene dine:");
-		
 		StringTokenizer st = new StringTokenizer(input, ",");
-		
+
 		for (int min = parseInt((String)st.nextElement()); st.countTokens() != 0;) {
-			
 			min = Math.min(min, parseInt((String)st.nextElement()));
-
-			if (st.countTokens() == 0) {
-			   showMessageDialog(null, "Tallet >> " + min + " << er det minste tallet av " + input);
-			   }
-		}		
+			if (st.countTokens() == 0) 	 showMessageDialog(null, min);
+		}
+		
+		
 	}
-
+	
 }
