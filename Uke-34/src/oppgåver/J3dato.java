0c7d50b05cd6d540ddfa0b99a869be73ced1f186
h669800 <111427835+h669800@users.noreply.github.com>
2022-09-03 15:40:03 +0200

    Update J3dato.java

--- Uke-34/src/oppgåver/J3dato.java
+++ Uke-34/src/oppgåver/J3dato.java
@@ -3,35 +3,47 @@ package oppgåver;
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
 public class J3dato {
-
+	
+	public static boolean isValid (String s, int minl, int maxl, int maxV) {
+		char[] characters = s.toCharArray();
+		
+		    for (int i = s.length() - 1; i != -1; i--) 
+				if (Character.isDigit(s.toCharArray()[i]) == false) 
+					return false;
+		    
+		   if (s.length() < minl || s.length() > maxl)
+			   return false;
+		   if (parseInt(s) < 1 || parseInt(s) > maxV)
+			   return false;
+		
+		return true;
+	}
 	public static void main(String[] args) {
 		
 		// for (int i = 1; i == 1; i--) {
 		// try {
+
 		   String Day = showInputDialog("Example: 03 \n Enter the Day:");
-	       while ( (Character.isDigit(Day.toCharArray()[0]) == false || Character.isDigit(Day.toCharArray()[1]) == false) || (Day.length() < 1 || Day.length() > 2) || (parseInt(Day) < 1 || parseInt(Day) > 31) )
-	            Day = showInputDialog("Example: 03 \n Enter the day: ");
+	       while ( (isValid(Day, 1, 2, 31) == false) )
+	            Day = showInputDialog("Example: 03 \n Error; enter the Day again: ");
 		
 		   String Month = showInputDialog("Example: 01 \n Enter the Month:");
-	       while ( (Character.isDigit(Month.toCharArray()[0]) == false || Character.isDigit(Month.toCharArray()[1]) == false) || (Month.length() < 1 || Month.length() > 2) || (parseInt(Month) < 1 || parseInt(Month) > 12) )
-	            Month = showInputDialog("Example: 01 \n Enter the Month: ");
+	       while ( (isValid(Month, 1, 2, 12) == false))
+	            Month = showInputDialog("Example: 01 \n Error; enter the Month again: ");
 		
 		   String Year = showInputDialog("Example: 2002 \n Enter the year:");
-		   while ( (Character.isDigit(Year.toCharArray()[0]) == false || Character.isDigit(Year.toCharArray()[1]) == false || Character.isDigit(Year.toCharArray()[3]) == false ||  Character.isDigit(Year.toCharArray()[3]) == false ) || Year.length() != 4) Year = showInputDialog("Years must have 4 numbers, example: 2002.");
+		   while ( ( isValid(Year, 1, 4, 9999) == false)) 
+			   Year = showInputDialog("Example: 2002 \n Error; enter the Year again:");
 
 		
 
 		         showMessageDialog(null, Day + "." + Month + "." + Year);
 		
 		// } catch (Exception Feil) {
-			
 		// 	showMessageDialog(null, "Feil: Du har ikke skrevet inn ett valid tall. Prøv på nytt.");
 		// 	i++;
 		// }
 		// }
-		
-		
-		
 	}
-
+	
 }
