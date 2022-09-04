45a88eb1b4849bca2b3b7401cd1b6946ce857791
h669800 <111427835+h669800@users.noreply.github.com>
2022-09-04 16:58:22 +0200

    Update J3dato.java

--- Uke-34/src/oppgåver/J3dato.java
+++ Uke-34/src/oppgåver/J3dato.java
@@ -3,47 +3,54 @@ package oppgåver;
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
 public class dato {
+
+	public static void main(String[] args) {
+
+
+		String Day = zeroedUserInput("Day", 1, 2, 31);
+		String Month = zeroedUserInput("Month", 1, 2, 12);
+		String Year = zeroedUserInput("Year", 1, 4, 9999);
+		// (text, minLength, maxLength, maxValue)
+			
+		   showMessageDialog(null, "Date: " + Day + "." + Month + "." + Year);
+		
+	}
+	
+	public static String zeroedUserInput (String text, int minLength, int maxLength, int maxValue) {
+		
+		String userInput;
+		do userInput = showInputDialog("Enter the " + text + ": ");
+	    while ( !isValid(userInput, minLength, maxLength, maxValue) ); 
+		// Asks the user for the value until it is Valid
+		   
+		while ( maxLength != userInput.length() )  userInput = "0" + userInput; 
+		// Add a zero behind the String/number (ex: 1 becomes 0001 if the maxlength is 4)
+		return userInput;
+	}
 	
 	public static boolean isValid (String s, int minLength, int maxLength, int maxValue) {
-
-
-		      for (int i = s.length() - 1; i != -1; i--) {
-		    	  // Run once for each character in string
-				   if ( Character.isDigit( s.toCharArray()[i] ) == false ) return false; 
-		      } // return false if string contains numbers
+		
+		
+		   if (s == null) System.exit(0);
+		   // exits program if you click cancel in the window
+		   for (int i = s.length() - 1; i != -1; i--) {
+		 	  // Run once for each character in string
+        	   if ( !Character.isDigit( s.charAt(i) ) ) return false; 
+		   } // return false if string contains letters
+		     
+		   try {
+			   
+		      int value = parseInt(s);
+		      if (value < 1 || value > maxValue)  return false;
+		      // If too high value, return false.
+		      if (s.length() < minLength || s.length() > maxLength) return false; 
+		      // if too many/few characters, return false.
 		      
-		   if (parseInt(s) < 1 || parseInt(s) > maxValue)  return false;
-		   // If too high value, return false.
-		   if (s.length() < minLength || s.length() > maxLength) return false; 
-		   // if too many/few characters, return false.
+		   } catch (Exception e) {
+					return false;
+		   } // return false if invalid elements are entered.
 		   
-	     return true; // if none of the if statements initiate
-	}
-	
-	public static String zeroedUserInput (String[] st, int minLength, int maxLength, int maxValue) {
-		
-		do st[1] = showInputDialog("Enter the " + st[0] + ": ");
-	        while ( isValid(st[1], minLength, maxLength, maxValue) == false ); 
-		// Asks the user to input the Day/Month/Year until isValid == true.
-		
-		while (maxLength != st[1].length())     st[1] = "0" + st[1]; 
-		// Add a zero behind the String/number (ex: 1 becomes 0001 if the maxlength is 4)
-		return st[1];
-	}
-	
-	public static void main(String[] args) {
-		
-		String[] Day = {"Day", "userInput"};
-		String[] Month = {"Month", "userInput"};
-		String[] Year = {"Year", "userInput"};
-		
-		Day[1] = zeroedUserInput(Day, 1, 2, 31);
-		Month[1] = zeroedUserInput(Month, 1, 2, 12);
-	        Year[1] = zeroedUserInput(Year, 1, 4, 9999);
-			
-		   
-		         showMessageDialog(null, "Date: " + Day[1] + "." + Month[1] + "." + Year[1]);
-		
+	return true; // if none of the if statements initiate
 	}
 	
 }
