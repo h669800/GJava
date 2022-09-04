d777f244b0d9407ae190058d156a7b00301a9691
h669800 <111427835+h669800@users.noreply.github.com>
2022-09-04 15:15:31 +0200

    Update J3dato.java

--- Uke-34/src/oppgåver/J3dato.java
+++ Uke-34/src/oppgåver/J3dato.java
@@ -2,48 +2,47 @@ package oppgåver;
 
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
-public class J3dato {
-		public static boolean isValid (String s, int minLength, int maxLength, int maxValue) {
-		char[] s_characters = s.toCharArray(); // create an array of each character in the string s.
+public class dato {
+	
+	public static boolean isValid (String s, int minLength, int maxLength, int maxValue) {
 
-		   for ( int i = s.length() - 1; i != -1; i-- ) { // The for loop runs once for each character in the string. (ex: if string is "Hello", it runs 5 times.)
-		      if ( Character.isDigit(  s_characters[i]  ) == false ) return false; // If any character in the string is not a number, return false. (String Hello12 returns false)
-		   }
-		   if (parseInt(s) < 1 || parseInt(s) > maxValue)  return false;// If too high value, return false. (ex: The 38th day of the month = false)
-		   if (s.length() < minLength || s.length() > maxLength) return false; // if too many/few characters, return false. (ex: The 114th month = false)
+
+		      for (int i = s.length() - 1; i != -1; i--) {
+		    	  // Run once for each character in string
+				   if ( Character.isDigit( s.toCharArray()[i] ) == false ) return false; 
+		      } // return false if string contains numbers
+		      
+		   if (parseInt(s) < 1 || parseInt(s) > maxValue)  return false;
+		   // If too high value, return false.
+		   if (s.length() < minLength || s.length() > maxLength) return false; 
+		   // if too many/few characters, return false.
 		   
-	     return true; // if none of the if statements initiate, return isValid(String, minLength, maxLength, maxValue) = true.
+	     return true; // if none of the if statements initiate
 	}
 	
-	public static String zeroes (String st, int maxLength) {
+	public static String zeroedUserInput (String[] st, int minLength, int maxLength, int maxValue) {
 		
-		while (maxLength != st.length())    st = "0" + st; // Add a zero behind the String/number (ex: 1 becomes 0001 if the maxlength is 4)
-		return st; // returns the string, so zeroes(String, maxLength) = st.
+		do st[1] = showInputDialog("Enter the " + st[0] + ": ");
+	        while ( isValid(st[1], minLength, maxLength, maxValue) == false ); 
+		// Asks the user to input the Day/Month/Year until isValid == true.
+		
+		while (maxLength != st[1].length())     st[1] = "0" + st[1]; 
+		// Add a zero behind the String/number (ex: 1 becomes 0001 if the maxlength is 4)
+		return st[1];
 	}
+	
 	public static void main(String[] args) {
 		
-		String Day;
-		String Month;
-		String Year;
-		// Declares the String's because we cannot do it in the loops below.
-
-		   do Day = showInputDialog("Example: 03 \n Enter the Day:");
-	       while ( isValid(Day, 1, 2, 31) == false ); // isValid (String st = Day; minLength = 1; maxLength = 2; maxValue = 31)
-	            
-		   do Month = showInputDialog("Example: 01 \n Enter the Month:");
-	       while ( isValid(Month, 1, 2, 12) == false ); // isValid (String st = Month; minLength = 1; maxLength = 2; maxValue = 12)
-	            
-		   do Year = showInputDialog("Example: 2002 \n Enter the year:");
-		   while ( isValid(Year, 1, 4, 9999) == false ); // isValid (String st = Year; minLength = 1; maxLength = 4; maxValue = 9999)
-		   // The while loop makes you enter the number until you enter a valid number or exit the program.
+		String[] Day = {"Day", "userInput"};
+		String[] Month = {"Month", "userInput"};
+		String[] Year = {"Year", "userInput"};
+		
+		Day[1] = zeroedUserInput(Day, 1, 2, 31);
+		Month[1] = zeroedUserInput(Month, 1, 2, 12);
+	        Year[1] = zeroedUserInput(Year, 1, 4, 9999);
+			
 		   
-		   
-		   Day = zeroes(Day, 2);
-		   Month = zeroes(Month, 2);
-		   Year = zeroes(Year, 4);
-			// Adds zeroes behind the number. (ex: Year 115 = 0115, to match 4 characters.)
-		   
-		         showMessageDialog(null, "Date: " + Day + "." + Month + "." + Year);
+		         showMessageDialog(null, "Date: " + Day[1] + "." + Month[1] + "." + Year[1]);
 		
 	}
 	
