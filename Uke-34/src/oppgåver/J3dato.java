acb84da641813405351fe31a2516c3924cbfffb1
h669800 <111427835+h669800@users.noreply.github.com>
2022-09-04 18:51:24 +0200

    Update J3dato.java

--- Uke-34/src/oppgåver/J3dato.java
+++ Uke-34/src/oppgåver/J3dato.java
@@ -1,8 +1,7 @@
 package oppgåver;
-
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
-public class dato {
+public class J3dato {
 
 	public static void main(String[] args) {
 
@@ -23,32 +22,31 @@ public class dato {
 	    while ( !isValid(userInput, minLength, maxLength, maxValue) ); 
 		// Asks the user for the value until it is Valid
 		   
-		while ( maxLength != userInput.length() )  userInput = "0" + userInput; 
+		while ( userInput.length() != maxLength )  userInput = "0" + userInput; 
 		// Add a zero behind the String/number (ex: 1 becomes 0001 if the maxlength is 4)
-		return userInput;
+		return userInput; 
 	}
 	
-	public static boolean isValid (String s, int minLength, int maxLength, int maxValue) {
+	public static boolean isValid (String userInput, int minLength, int maxLength, int maxValue) {
 		
 		
-		   if (s == null) System.exit(0);
+		   if (userInput == null) System.exit(0);
 		   // exits program if you click cancel in the window
-		   for (int i = s.length() - 1; i != -1; i--) {
+		   for (int i = userInput.length() - 1; i != -1; i--) {
 		 	  // Run once for each character in string
-        	   if ( !Character.isDigit( s.charAt(i) ) ) return false; 
+        	   if ( !Character.isDigit( userInput.charAt(i) ) ) return false; 
 		   } // return false if string contains letters
 		     
 		   try {
 			   
-		      int value = parseInt(s);
+		      int value = parseInt(userInput);
 		      if (value < 1 || value > maxValue)  return false;
 		      // If too high value, return false.
-		      if (s.length() < minLength || s.length() > maxLength) return false; 
+		      if (userInput.length() < minLength || userInput.length() > maxLength) return false; 
 		      // if too many/few characters, return false.
 		      
-		   } catch (Exception e) {
-					return false;
-		   } // return false if invalid elements are entered.
+		   } catch (Exception e) { return false; } 
+		   // return false if invalid elements are entered.
 		   
 	return true; // if none of the if statements initiate
 	}
